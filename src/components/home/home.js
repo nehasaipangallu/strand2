de55796dcfc06627d1cb15afb7705754d3a53de0
nehasaipangallu <52908091+nehasaipangallu@users.noreply.github.com>
2022-12-13 01:00:56 +0530

    style enhancements

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -6,12 +6,12 @@ export default function Home() {
     <div class="home-page d-flex justify-content-end align-items-center">
       <div class="home-page-container">
         <h3> Get Health Insights Right</h3>
-        <p>
+        <p class="para">
           Vivamus elementum dictum augue at fermentum. Proin non nisi at quam
           sollicitudin consectetur. Suspendisse tempus at diam at volutpat.
           Donec sit amet dui malesuada, tincidunt sem sed, tincidunt justo.
         </p>
-        <button className="btn btn-warning"> View Insights</button>
+        <button className="btn btn-view"> View Insights</button>
       </div>
     </div>
   );
