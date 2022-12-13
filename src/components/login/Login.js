1d813e652fb3e87beaf2fcd4d21571d8146cddd4
nehasaipangallu <52908091+nehasaipangallu@users.noreply.github.com>
2022-12-13 08:28:52 +0530

    log in style enhancements

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -16,7 +16,12 @@ export default class Login extends React.Component {
     return (
       <div class="login-page d-flex justify-content-end align-items-center">
         <div class="login-page-container">
-          <div class="login-background d-flex flex-column justify-content-center">
+          <div
+            class="login-background flex-column justify-content-center"
+            style={{
+              display: !this.state.isOtpSent ? 'flex' : 'none',
+            }}
+          >
             <h3> Continue with Phone Number</h3>
             <h5>Phone Number</h5>
             <input
@@ -29,7 +34,12 @@ export default class Login extends React.Component {
             </button>
           </div>
 
-          <div class="otp-background d-flex flex-column justify-content-center ">
+          <div
+            class="otp-background flex-column justify-content-center"
+            style={{
+              display: this.state.isOtpSent ? 'flex' : 'none',
+            }}
+          >
             <h3> Continue with Phone Number</h3>
             <h5>Phone Number</h5>
             <input
