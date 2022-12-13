14319ec7edcaa7a77e578b33689602e94045120f
nehasaipangallu <52908091+nehasaipangallu@users.noreply.github.com>
2022-12-13 17:07:47 +0530

    Home page class name is modified

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -3,10 +3,10 @@ import './home.css';
 
 export default function Home() {
   return (
-    <div class="home-page d-flex justify-content-end align-items-center">
-      <div class="home-page-container">
+    <div className="home-page d-flex justify-content-end align-items-center">
+      <div className="home-page-container">
         <h3> Get Health Insights Right</h3>
-        <p class="para">
+        <p className="para">
           Vivamus elementum dictum augue at fermentum. Proin non nisi at quam
           sollicitudin consectetur. Suspendisse tempus at diam at volutpat.
           Donec sit amet dui malesuada, tincidunt sem sed, tincidunt justo.
