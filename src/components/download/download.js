d1b1d2c7a27c215f49be2e8c8e7d2abd8d20ce4d
nehasaipangallu <52908091+nehasaipangallu@users.noreply.github.com>
2022-12-18 11:29:35 +0530

    download page style enhancement

--- src/components/download/download.js
+++ src/components/download/download.js
@@ -13,12 +13,12 @@ export default class Download extends React.Component {
         <table class="table download-table">
           <thead>
             <tr>
-              <th scope="col">Health Insights</th>
-              <th scope="col">Sample Collection Date</th>
-              <th scope="col">Report Date</th>
-              <th scope="col">Report Status</th>
-              <th scope="col">View Report</th>
-              <th scope="col">Download Report</th>
+              <th style={{ width: '20%' }}>Health Insights</th>
+              <th style={{ width: '20%' }}>Sample Collection Date</th>
+              <th style={{ width: '15%' }}>Report Date</th>
+              <th style={{ width: '15%' }}>Report Status</th>
+              <th style={{ width: '15%' }}>View Report</th>
+              <th style={{ width: '15%' }}>Download Report</th>
             </tr>
           </thead>
           <tbody>
@@ -27,24 +27,56 @@ export default class Download extends React.Component {
               <td>12/08/2022</td>
               <td>20/08/2022</td>
               <td>Positive</td>
-              <td>Otto</td>
-              <td>@mdo</td>
+              <td class="icon-report">
+                <img
+                  alt=""
+                  src="https://github.com/nehasaipangallu/strand2/blob/main/public/report%20orange.svg?raw=true"
+                />
+              </td>
+              <td class="icon-download">
+                {' '}
+                <img
+                  alt=""
+                  src="https://github.com/nehasaipangallu/strand2/blob/main/public/download%20orange.svg?raw=true"
+                />
+              </td>
             </tr>
             <tr>
               <td>Cardio</td>
               <td>12/08/2022</td>
               <td>20/08/2022</td>
               <td>Negative</td>
-              <td>Otto</td>
-              <td>@mdo</td>
+              <td class="icon-report">
+                <img
+                  alt=""
+                  src="https://github.com/nehasaipangallu/strand2/blob/main/public/report%20orange.svg?raw=true"
+                />
+              </td>
+              <td class="icon-download">
+                {' '}
+                <img
+                  alt=""
+                  src="https://github.com/nehasaipangallu/strand2/blob/main/public/download%20orange.svg?raw=true"
+                />
+              </td>
             </tr>
             <tr>
               <td>Carrier Risk</td>
               <td>20/06/2022</td>
               <td>28/06/2022</td>
               <td>Positive</td>
-              <td>Otto</td>
-              <td>@mdo</td>
+              <td class="icon-report">
+                <img
+                  alt=""
+                  src="https://github.com/nehasaipangallu/strand2/blob/main/public/report%20orange.svg?raw=true"
+                />
+              </td>
+              <td class="icon-download">
+                <img
+                  alt=""
+                  src="https://github.com/nehasaipangallu/strand2/blob/main/public/download%20orange.svg?raw=true"
+                />
+              </td>
             </tr>
           </tbody>
         </table>
