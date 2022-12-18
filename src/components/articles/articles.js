c6daa95a258f38cc6bd7f7f263f15ef9fe3c0afe
nehasaipangallu <52908091+nehasaipangallu@users.noreply.github.com>
2022-12-18 11:49:48 +0530

    header in articles pager

--- src/components/articles/articles.js
+++ src/components/articles/articles.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './articles.css';
 
-export class Articles extends React.Component {
+export default class Articles extends React.Component {
   constructor() {
     super();
     this.state = {};
   }
   render() {
-    return <div class="articles-container">Welcomes</div>;
+    return (
+      <div class="articles-container m-3">
+        <div class="p-5">
+          <div class="d-flex justify-content-between header">
+            <h1 class="header-text">All Articles</h1>
+            <div class="search-container">
+              <input type="text" placeholder="Search" />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
