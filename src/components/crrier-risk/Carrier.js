56d523b084ff8692bb5eda786aba78cde98f87f9
nehasaipangallu <52908091+nehasaipangallu@users.noreply.github.com>
2022-12-20 10:57:59 +0530

    Carrier page content is added

--- src/components/crrier-risk/Carrier.js
+++ src/components/crrier-risk/Carrier.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Carrier.css';
 import { Link, useNavigate } from 'react-router-dom';
 import Carousel from 'react-bootstrap/Carousel';
+import Table from 'react-bootstrap/Table';
 
 export default function Carrier(props) {
   props.onLoad(true);
@@ -12,7 +13,7 @@ export default function Carrier(props) {
   }
   return (
     <div className="d-flex flex-column mb-3">
-      <div className="p-2 cardio-rep-box">
+      <div className="p-2 carrier-rep-box">
         <div className="d-flex flex-row mb-3">
           <div className="p-2">
             <img
@@ -20,7 +21,7 @@ export default function Carrier(props) {
               className="carrier-img"
             />
           </div>
-          <p className="p-2">
+          <p className="p-2 carrier-para">
             Your Report is Ready. Please click to View Report.
           </p>
           <div className=" d-flex flex-column report-dates align-self-center">
@@ -63,6 +64,128 @@ export default function Carrier(props) {
       65% of which diseases associated with a reduced lifespan² and affect the quality of life significantly
       </div> */}
     </div>
+    <div className="p-2 type-carrier">
+      <h1 className ="type-head">
+      Types of carrier risk disorder
+      </h1>
+      <div className="d-flex flex-row mb-3">
+        <div className="ar-disorder">
+          <h4 className="ar-head">A R Disorders</h4>
+          <p className="ml-5">
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+          </p>
+        </div>
+        <div className="ar-disorder-2">
+          <h4 className="ar-head">X-linked Disorders</h4>
+          <p>
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+          </p>
+        </div>
+      </div>
+      </div>
+      <div className="p-2 table-risk">
+      <div className="m-4">
+          <Table responsive="sm">
+            <tbody>
+              <tr>
+                <td>Disease</td>
+                <td>Frequency per 1000,000</td>
+                <td>Total NUmbers in India</td>
+                <td>Inheritance</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+            </tbody>
+          </Table>
+      </div>
+     </div>
+     <div className="p-2">
+     <Carousel>
+          <Carousel.Item>
+            <div className="d-flex flex-row mb-3 flex-car justify-content-between align-items-center">
+              <div className="p-2 car-img-item">
+                <img
+                  src="https://github.com/nehasaipangallu/strand2/blob/main/public/cardiomypathies.svg?raw=true"
+                  alt="First slide"
+                  className="tumor-img"
+                />
+              </div>
+              <div className="p-5 carousel-cap">
+                <h3>Cardiomypathies</h3>
+                <p>
+                  Cardiomyopathy (kahr-dee-o-my-OP-uh-thee) is a disease of the
+                  heart muscle that makes it harder for the heart to pump blood
+                  to the rest of the body. Cardiomyopathy can lead to heart
+                  failure. The main types of cardiomyopathy include dilated,
+                  hypertrophic and restrictive cardiomyopathy.
+                </p>
+              </div>
+            </div>
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <div className="d-flex flex-row mb-3 flex-car justify-content-end align-items-center">
+              <div className="p-2 car-img-item">
+                <img
+                  src="https://github.com/nehasaipangallu/strand2/blob/main/public/Group%2048095702.svg?raw=true"
+                  alt="Second slide"
+                  className="arhy-img"
+                />
+              </div>
+              <div className="p-5 carousel-cap">
+                <h3>Arrhythmias</h3>
+                <p>
+                  A heart arrhythmia (uh-RITH-me-uh) is an irregular heartbeat.
+                  Heart rhythm problems (heart arrhythmias) occur when the
+                  electrical signals that coordinate the heart's beats don't
+                  work properly. The faulty signaling causes the heart to beat
+                  too fast (tachycardia), too slow (bradycardia) or irregularly.
+                </p>
+              </div>
+            </div>
+          </Carousel.Item>
+        </Carousel>
+     </div>
     </div>
   );
 }
