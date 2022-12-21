b443f3ef53be1cab0f8c54b164e866eadb379dd1
nehasaipangallu <52908091+nehasaipangallu@users.noreply.github.com>
2022-12-21 08:36:56 +0530

    adult page style improvements

--- src/components/adult-onset/adult-onset.js
+++ src/components/adult-onset/adult-onset.js
@@ -20,43 +20,45 @@ export default class AdultOnset extends React.Component {
           />
         </div>
         <div class="d-flex adult-route-container m-5 flex-wrap">
-          <div class="route-row-1 d-flex flex-row justify-content-center flex-wrap">
+          <div class="route-row-1 m-3 d-flex flex-column justify-content-evenly flex-wrap">
             <h4 class="row-1-heading">Adult Onset</h4>
-            <div class="row-1-card m-3">
-              <img src="https://github.com/nehasaipangallu/strand2/blob/main/public/cancer.svg?raw=true" />
-              <h3>Cancer</h3>
-              <button
-                class="btn btn-report m-2"
-                onClick={this.onVeiwReortClick}
-              >
-                View Report
-              </button>
-            </div>
-            <div class="row-1-card m-3">
-              <img src="https://github.com/nehasaipangallu/strand2/blob/main/public/cardio.svg?raw=true" />
-              <h3>Cardio</h3>
-              <button
-                class="btn btn-report m-2"
-                onClick={this.onVeiwReortClick}
-              >
-                View Report
-              </button>
-            </div>
-            <div class="row-1-card m-3">
-              <img src="https://github.com/nehasaipangallu/strand2/blob/main/public/metabolism.svg?raw=true" />
-              <h3>Metabolic</h3>
-              <button
-                class="btn btn-report m-2"
-                onClick={this.onVeiwReortClick}
-              >
-                View Report
-              </button>
+            <div class="d-flex flex-row justify-content-evenly">
+              <div class="row-1-card m-3">
+                <img src="https://github.com/nehasaipangallu/strand2/blob/main/public/cancer.svg?raw=true" />
+                <h3>Cancer</h3>
+                <button
+                  class="btn btn-report m-2"
+                  onClick={this.onVeiwReortClick}
+                >
+                  View Report
+                </button>
+              </div>
+              <div class="row-1-card m-3">
+                <img src="https://github.com/nehasaipangallu/strand2/blob/main/public/cardio.svg?raw=true" />
+                <h3>Cardio</h3>
+                <button
+                  class="btn btn-report m-2"
+                  onClick={this.onVeiwReortClick}
+                >
+                  View Report
+                </button>
+              </div>
+              <div class="row-1-card m-3">
+                <img src="https://github.com/nehasaipangallu/strand2/blob/main/public/metabolism.svg?raw=true" />
+                <h3>Metabolic</h3>
+                <button
+                  class="btn btn-report m-2"
+                  onClick={this.onVeiwReortClick}
+                >
+                  View Report
+                </button>
+              </div>
             </div>
           </div>
-          <div class="route-row-2 d-flex flex-row justify-content-center flex-wrap">
+          <div class="route-row-2 d-flex flex-row justify-content-evenly flex-wrap">
             <div class="d-flex flex-column">
               <h4 class="row-2-heading">Carrier Risk</h4>
-              <div class="row-2-card m-3">
+              <div class="row-2-card m-4">
                 <img src="https://github.com/nehasaipangallu/strand2/blob/main/public/spreading%201.svg?raw=true" />
                 <h3>Cancer</h3>
                 <button
@@ -69,7 +71,7 @@ export default class AdultOnset extends React.Component {
             </div>
             <div class="d-flex flex-column">
               <h4 class="row-2-heading">Pharmacogenomics</h4>
-              <div class="row-2-card m-3">
+              <div class="row-2-card m-4">
                 <img src="https://github.com/nehasaipangallu/strand2/blob/main/public/StrandBrandGuide-14%202.svg?raw=true" />
                 <h3>Cardio</h3>
                 <button
